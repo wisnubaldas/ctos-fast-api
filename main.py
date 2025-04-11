cb96d19a965d82b9b3b1f0d5ccdd1ed59abfcc1f
wisnuhidayat <wisnubaldas@gmail.com>
2025-04-11 15:16:55 +0700

    baru

---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import  BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}\
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
